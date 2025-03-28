4515e7007c5f3310391da773704f4d654aec2aec
OlegR <oleg7309@inbox.lv>
2025-03-29 00:01:08 +0200

    Error fix

--- server/index.js
+++ server/index.js
@@ -31,7 +31,8 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log(' Backend listening at http://localhost:5000');
 });
 
