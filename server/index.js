5a70fa6f49c8e89777f607b0395f2ff2ab15ef37
OlegR <oleg7309@inbox.lv>
2025-03-29 00:17:29 +0200

    fix3

--- server/index.js
+++ server/index.js
@@ -5,9 +5,16 @@ const cors = require('cors');
 const OpenAI = require('openai');
 
 const app = express();
+const path = require('path');
+
 app.use(cors());
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
