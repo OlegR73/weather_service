43418a12a5fbf5d01f768a2efaf84ff14e451654
OlegR <oleg7309@inbox.lv>
2025-03-29 00:13:40 +0200

    error fix2

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const OpenAI = require('openai');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
